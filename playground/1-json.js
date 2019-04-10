74dd1668b8742c2f19fbff4571b15677a14349d6
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-10 10:53:08 +0500

    creating json file take data stringify it parse it read from file change buffered data after read from file and overwrite file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 
-const myTeacher = {
-        name: "Andrew",
-        planet: "Earth",
-        age: 27
-    };
-const teacher  = JSON.stringify(myTeacher);
-fs.writeFileSync('1-json.json', teacher);
+// const myTeacher = {
+//     name: "Andrew",
+//     planet: "Earth",
+//     age: 27
+// };
+// const teacher  = JSON.stringify(myTeacher);
+const dataBuffer = fs.readFileSync('1-json.json');
+// console.log(dataBuffer); // bring buffered data from file
 
+const stringData = dataBuffer.toString();
+const parsedData = JSON.parse(stringData);
+parsedData.name = 'Shakhboz';
+parsedData.age = 25;
+console.log(parsedData);
+stringValue = JSON.stringify(parsedData);
+fs.writeFileSync('1-json.json', stringValue);
