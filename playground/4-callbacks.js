4c24cf586e5828d3a0c0c6ea696d34f618898056
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-16 16:46:16 +0500

    asyncronous function

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -13,5 +13,25 @@ const geocode = (addr, callback) => { //fully Asyncronous function
 };
 
 geocode('Tashkent', (data) => {
-   console.log(data);
+    console.log(data);
+});
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (arg1, arg2, sum) => {
+    setTimeout(() => {
+        sum1 = arg1 + arg2;
+        sum(sum1);
+    }, 2000);
+
+};
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
 });
