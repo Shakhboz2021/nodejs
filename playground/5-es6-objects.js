5382d9d69f0d1d941729c3e1aa688355dc69199b
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-17 14:46:46 +0500

    ES6 Object propeties

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,31 @@
+// Object property shorthand
+
+const name = 'Shakhboz';
+const userAge = 25;
+
+const user = {
+    name, // It is same syntax with => name: name, it is called object property shorthand
+    age: userAge,
+    location: 'Tashkent'
+};
+console.log(user);
+
+// Object destructing
+
+const product = {
+    label: 'Red Notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+    rating: 4
+};
+
+// const label = product.label;
+// const stock = product.stock;
+
+const {label: productName, stock, rating = 5} = product;
+
+// console.log(label);
+console.log(productName); // can change name of object property
+console.log(stock);
+console.log(rating);
