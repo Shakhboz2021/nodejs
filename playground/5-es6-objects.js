b5026115c07ddfec209a3bff63e4746172c8ace3
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-17 14:51:25 +0500

    ES6 Object propeties in function

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -29,3 +29,9 @@ const {label: productName, stock, rating = 5} = product;
 console.log(productName); // can change name of object property
 console.log(stock);
 console.log(rating);
+
+const transaction = (type, {label, stock}) => { // object property can be used as an argument
+    console.log(type, label, stock);
+};
+
+transaction('order', product);
