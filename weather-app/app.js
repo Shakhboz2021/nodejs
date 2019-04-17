bec28b6fa41a5db0af5eeac5d88b30b166601a9f
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-17 11:57:49 +0500

    access in command line commands

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,26 @@
-const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+const addr = process.argv[2];// accessing command line elements
+if (!addr) {
+    console.log('Please, enter address!')
+} else {
+    geocode(addr, (error, data) => {
+        if (error) {
+            console.log(error);
+        } else {
+            forecast(data.latitude, data.longitude, (error, dataForecast) => {// callback chaining
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('It is currently ' + dataForecast.temperature + ' Celsius in ' + data.location);
+                    console.log('There is a ' + dataForecast.chanceOfRain + ' chance of rain in ' + data.location);
+                }
+
+            });
+        }
+    });
+}
 
 
-geocode('Tashkent', (error, data) => {
-    if (error !== undefined)  {
-        console.log(error);
-    } else {
-        forecast(data.latitude, data.longitude, (error, dataForecast) => {
-            if (error !== undefined) {
-                console.log(error);
-            } else {
-                console.log('It is currently ' + dataForecast.temperature +' Celsius in ' + data.location);
-                console.log('There is a ' + dataForecast.chanceOfRain +' chance of rain in ' + data.location);
-            }
-
-        });
-    }
-});
 
