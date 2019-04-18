19762236a87e2141909317025a46fc1063ce58ac
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-18 11:24:18 +0500

    handle bars (.hbs) added to the project in order to load pages with dynamic content

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,35 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
 const app = express();
 
 const publicDirectoryName = path.join(__dirname, '../public');
+app.set('view engine', 'hbs');
+
 app.use(express.static(publicDirectoryName));
 
-// app.get('', (req, res) => {
-//    res.send('<h1>Hello, it is my first server ever!</h1>')
-// });
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'My name',
+        name: 'Shakhboz'
+    })
+});
 
-app.get('/weather', (req, res) => {
-   res.send({
-      forecast: 'rainy',
-      location: 'Tashkent'
-   })
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Shakhboz Tokhirov'
+    })
+});
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'How can I help you?',
+        name: 'Shakhboz Tokhirov'
+    })
 });
 
 app.listen(4000, () => {
-   console.log('Server is up on port 4000.')
+    console.log('Server is up on port 4000.')
 });
