a49bae55acce3a5b726d4bebf706b3644ffab9b6
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-18 12:23:14 +0500

    Advanced templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 // console.log(__dirname);
 // console.log(__filename);
@@ -8,13 +9,14 @@ const app = express();
 
 //Define paths for Express config
 const publicDirectoryName = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
-
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryName));
