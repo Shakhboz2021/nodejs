538907fd63057ca0a31a66265cd4a7962766c268
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-24 11:15:35 +0500

    Inserting single document into MongoDB

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -6,17 +6,22 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1/27017';
 const databaseName = 'task-manager';
 
-MongoClient.connect(connectionURL, { 'useNewUrlParser': true }, (error, client) => {
-   if (error) {
-       return console.log('Unable to connect to the database!')
-   }
+MongoClient.connect(connectionURL, {'useNewUrlParser': true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to the database!')
+    }
 
-   // console.log('Connected correctly!')
+    console.log('Connected correctly!');
     const db = client.db(databaseName);
 
-   db.collection('users').insertOne({
-       name: 'Shakhboz',
-       age: 25
-   })
+    db.collection('users').insertOne({
+        name: 'Shakhboz',
+        age: 25
+    },(error, result) => {
+        if (error) {
+            return console.log('Unable to insert user')
+        }
+        console.log(result.ops)
+    })
 
 });
