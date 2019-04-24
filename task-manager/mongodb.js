48993eb47572fe0b851a29efcf2d0ddc6227a21b
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-04-24 12:20:11 +0500

    MongoDB CRUD --> create step

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,14 +14,58 @@ MongoClient.connect(connectionURL, {'useNewUrlParser': true}, (error, client) =>
     console.log('Connected correctly!');
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Shakhboz',
-        age: 25
-    },(error, result) => {
+    /* db.collection('users').insertOne({
+         name: 'Shakhboz',
+         age: 25
+     },(error, result) => {
+         if (error) {
+             return console.log('Unable to insert user')
+         }
+         console.log(result.ops)
+     })*/
+
+    /*db.collection('user').drop((error, result) => {
         if (error) {
-            return console.log('Unable to insert user')
+            return console.log(error)
         }
+
+        console.log("Collection successfully dropped")
+    });*/
+    /* db.collection('users').insertMany([
+         {
+             name: "Mirodil",
+             age: 23
+         }, {
+             name: "Suxrob",
+             age: 23
+         }, {
+             name: "Navruzbek",
+             age: 22
+         }
+     ], (error, result) => {
+         if (error) {
+             return console.log(error)
+         }
+
+         console.log(result.ops)
+     })*/
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Make login page',
+            completed: true
+        }, {
+            description: 'Make registration page',
+            completed: false
+        }, {
+            description: 'Make forgot password page',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log(error)
+        }
+
         console.log(result.ops)
     })
-
 });
