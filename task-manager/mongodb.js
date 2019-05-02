bfb41218d01bd1bdfa1b80caaabac05eaec5de21
Shakhboz2021 <shakhboztokhirov@gmail.com>
2019-05-02 14:30:19 +0500

    data validation and sanitization

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,7 +1,11 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+//const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = require('mongodb');
+
+const {MongoClient, ObjectID} = require('mongodb');
+
 
 const connectionURL = 'mongodb://127.0.0.1/27017';
 const databaseName = 'task-manager';
@@ -50,22 +54,65 @@ MongoClient.connect(connectionURL, {'useNewUrlParser': true}, (error, client) =>
          console.log(result.ops)
      })*/
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Make login page',
-            completed: true
-        }, {
-            description: 'Make registration page',
-            completed: false
-        }, {
-            description: 'Make forgot password page',
-            completed: false
-        }
-    ], (error, result) => {
+    /* db.collection('tasks').insertMany([
+         {
+             description: 'Make login page',
+             completed: true
+         }, {
+             description: 'Make registration page',
+             completed: false
+         }, {
+             description: 'Make forgot password page',
+             completed: false
+         }
+     ], (error, result) => {
+         if (error) {
+             return console.log(error)
+         }
+
+         console.log(result.ops)
+     })*/
+
+    /*db.collection('users').findOne({
+        name: "Mirodil"
+    }, (error, user) => {
         if (error) {
             return console.log(error)
         }
 
-        console.log(result.ops)
+        console.log(user)
+    })*/
+
+    /*db.collection('users').find({age: 23}).toArray((error, users) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(users)
+    });*/
+    /* db.collection('users').find({age: 23}).count((error, userCount) => {
+         if (error) {
+             return console.log(error)
+         }
+         console.log(userCount)
+     })*/
+
+    /* db.collection('tasks').findOne({
+         _id: new ObjectID('5cc00b38e262bb266c542b93')
+     }, (error, task) => {
+         if (error) {
+             return console.log(error)
+         }
+         console.log(task)
+     })*/
+
+    db.collection('tasks').find({
+        completed: false
+    }).toArray((error, uncompletedTasks) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log(uncompletedTasks)
     })
+
 });
